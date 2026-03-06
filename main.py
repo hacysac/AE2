ff553f912b70c902d90b29eb4505f4a02e77b022
Hayley Chechel <hayley.chechel@gmail.com>
2026-03-06 08:39:42 +0000

    editted main file

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from webtris_client import APIClient, APIConnector, SingleSite
 
-
-def main():
-    connector = APIConnector()
-    client = APIClient(connector=connector)
-    site = SingleSite(site_id=461, site_name="Example Site")
-    day = "01"
-    month = "01"
-    year = "2024"
-    date = day + month + year
-    site.fetch_data(client, date)
-    print(f"Average speed for {date}: {site.calculate_avg_speed()}")
-    print(f"Total volume for {date}: {site.calculate_total_volume()}")
-    hour = site.find_peak_hour()
-    print(f"Peak hour: {hour}")
-    print(f"Average speed for hour {hour}: {site.calculate_avg_speed_for_hour(hour)}")
-    print(f"Total volume for hour {hour}: {site.calculate_total_volume_for_hour(hour)}")
-
-
-if __name__ == "__main__":
-    main()
+connector = APIConnector()
+client = APIClient(connector=connector)
+site = SingleSite(site_id=461, site_name="Example Site")
+day = "01"
+month = "01"
+year = "2024"
+date = day + month + year
+site.fetch_data(client, date)
+print(f"Average speed for {date}: {site.calculate_avg_speed()}")
+print(f"Total volume for {date}: {site.calculate_total_volume()}")
+hour = site.find_peak_hour()
+print(f"Peak hour: {hour}")
+print(f"Average speed for hour {hour}: {site.calculate_avg_speed_for_hour(hour)}")
+print(f"Total volume for hour {hour}: {site.calculate_total_volume_for_hour(hour)}")
